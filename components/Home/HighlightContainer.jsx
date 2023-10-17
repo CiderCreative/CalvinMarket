e21da530193b8fc63cb14aa57de53be94cc34599
LoganHumphrey <113280028+lhumphrey3511@users.noreply.github.com>
2023-10-17 10:32:17 -0400

    Working on grid sizing for home page

--- components/Home/HighlightContainer.jsx
+++ components/Home/HighlightContainer.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ItemHighlight } from './index'
+
 
 const HighlightContainer = ({text}) => {
   return (
     <div className="p-10">
       <h3 className="text-2xl font-bold mb-5">{text}</h3>
-      <div className="grid grid-cols-4 gap-10">
+      <div className={`grid grid-cols-${calculateGridItemsPerRow(useState.screenWidth)} gap-10`}>
         <ItemHighlight price="33" title="Math 251 Textbook" detail="978-1234567890" />
         <ItemHighlight price="72" title="Vintage Watch" detail="978-9876543210" />
         <ItemHighlight price="0" title="House Plant" detail="5/12/2022" />
@@ -16,3 +17,21 @@ const HighlightContainer = ({text}) => {
 }
 
 export default HighlightContainer
+
+function calculateGridItemsPerRow(screenWidth) {
+  let itemSize;
+
+  // Determine ItemHighlight size based on screen width
+  if (screenWidth >= 1024) {
+    itemSize = 200; // Large screens
+  } else if (screenWidth >= 768) {
+    itemSize = 150; // Medium screens
+  } else {
+    itemSize = 100; // Small screens
+  }
+
+  // Calculate the number of items that can fit per row
+  const itemsPerRow = Math.floor(screenWidth / itemSize);
+  console.log(itemsPerRow)
+  return itemsPerRow;
+}
--- components/Home/ItemHighlight.jsx
+++ components/Home/ItemHighlight.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ItemHighlight = ({price, title, detail}) => {
   return (
-    <div className="flex flex-col hover:scale-105 transition-transform duration-200 ease-in-out hover:cursor-pointer">
-      <div className="bg-maroon aspect-square w-[200px] flex-shrink-0" />
+    <div className="flex flex-col hover:scale-[102%] transition-transform duration-200 ease-in-out hover:cursor-pointer">
+      <div className="bg-maroon aspect-square w-[150px] lg:w-[200px] flex-shrink-0" />
       <p className="text-md leading-5 pt-1">{price > 0 ? `$${price}` : "Free"}</p>
       <p className="text-md font-bold">{title}</p>
       <p className="text-xs font-light">{detail}</p>
