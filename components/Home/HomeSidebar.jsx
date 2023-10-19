ecde82e857c64edd4dc4b2654464bbab42062609
LoganHumphrey <113280028+lhumphrey3511@users.noreply.github.com>
2023-10-19 09:27:23 -0400

    Send to main

--- components/Home/HomeSidebar.jsx
+++ components/Home/HomeSidebar.jsx
@@ -6,7 +6,7 @@ const HomeSidebar = ({sidebarClosed, setSidebarClosed}) => {
   const arrow = <svg className={`flex-shrink-0 transition-all duration-200 ${sidebarClosed ? "w-4 rotate-0" : "w-5 rotate-180"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m7 9 4-4-4-4M1 9l4-4-4-4"/></svg>
 
   return (
-    <div className={`fixed top-0 left-0 text-primary bg-primary w-[300px] drop-shadow-xl h-full ${sidebarClosed ? "w-[30px]" : ""} transition-all duration-300 overflow-hidden`}>
+    <div className={`max-sm:hidden fixed top-0 left-0 text-primary bg-primary w-[300px] drop-shadow-xl h-full ${sidebarClosed ? "w-[30px]" : ""} transition-all duration-300 overflow-hidden`}>
       <div className="flex text-primary mt-3 mr-2 justify-end hover:cursor-pointer" onClick={() => {setSidebarClosed(!sidebarClosed)}}>{arrow}</div>
       <div className={`${sidebarClosed ? "hidden" : ""}`}>
         <Logo />
