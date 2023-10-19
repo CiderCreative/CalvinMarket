a4116d083647b4f0a273765969e1a077162bea26
blint33 <lintbraden@gmail.com>
2023-10-19 12:40:23 -0400

    taking out amplify more

--- app/SignUp/page.jsx
+++ app/SignUp/page.jsx
@@ -1,18 +1,12 @@
 
 "use client"
-import { Amplify } from 'aws-amplify';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from '@/app/aws-exports';
-Amplify.configure(awsExports);
-
-function Page({ signOut, user }) {
+function Page) {
   return (
     <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
+      <h1>Hello</h1>
+      <button o>Sign out</button>
     </>
   );
 }
