217190f5fbc8a807fbe911d981b87f5aa2d3e42c
LoganHumphrey <113280028+lhumphrey3511@users.noreply.github.com>
2023-10-24 14:18:04 -0400

    Another war (won). This time with relation to the Carousel. Very good progress made.

--- components/Item/Carousel.jsx
+++ components/Item/Carousel.jsx
@@ -2,25 +2,17 @@
 
 import React, { useState, useEffect } from 'react'
 
-const Carousel = () => {
-  var count = 0;
-
-  const handleLeft = () => {
-    count = (count - 1 + colors.length) % colors.length;
-    const container = document.getElementById("container");
-    const rightPosition = count * 100 + "%";
-    container.style.right = rightPosition;
-  };
-
-  const handleRight = () => {
-    count = (count + 1) % colors.length;
-    const container = document.getElementById("container");
-    const rightPosition = count * 100 + "%";
-    container.style.right = rightPosition;
-  };
+const Carousel = ({colors}) => {
+  const [count, setCount] = useState(0)
+  const [distanceRight, setDistanceRight] = useState(0);
+  const handleLeft = () => { setCount((count - 1 + colors.length) % colors.length); };
+  const handleRight = () => { setCount((count + 1) % colors.length); };
 
 // Scroll on keypress
 useEffect(() => {
+  const rightPosition = count * 100 + "%";
+  setDistanceRight(rightPosition);
+
   const handleKeyDown = (e) => {
     switch (e.key) {
       case "ArrowLeft":
@@ -35,27 +27,23 @@ useEffect(() => {
         break;
     }
   };
-
-  window.addEventListener('keydown', handleKeyDown);
-
+  window.addEventListener('keydown', handleKeyDown); //Listen for Arrows Keys
   return () => {
     window.removeEventListener('keydown', handleKeyDown);
   };
-}, []);
-
-  const colors = [ "bg-[#123456]", "bg-[#AABBCC]", "bg-[#FF9900]", "bg-[#00FF00]", "bg-[#FF0000]", "bg-[#6600CC]", "bg-[#0099FF]", "bg-[#992233]", "bg-[#00CC99]" ];
+}, [count]);
 
   return (
 
     <div className='bg-dark relative w-[100%] h-full'>
 
-      <div className="flex overflow-visible [&>*]:flex-shrink-0 relative transition-all duration-200 right-0" id='container'>
-        {/* Create review block for each review */}
+      <div className="flex overflow-visible [&>*]:flex-shrink-0 relative transition-all duration-200 right-0" style={{right: distanceRight}}>
+        {/* Create block for each review */}
         {colors.map((style, idx) => <div key={idx} className={`w-full h-[500px] ${style}`} />)}
       </div>
 
-      <button className="absolute top-[50%] left-3 sm:left-[3vw] xl:left-[15vw] text-sm sm:text-lg lg:text-2xl aspect-square w-8 sm:w-[50px] rounded-full text-white hover:border-2 border-white active:scale-90" onClick={handleLeft}>←</button>
-      <button className="absolute top-[50%] right-3 sm:right-[3vw] xl:right-[15vw] text-sm sm:text-lg lg:text-2xl aspect-square w-8 sm:w-[50px] rounded-full text-white hover:border-2 border-white active:scale-90" onClick={handleRight}>→</button>
+      <button className="absolute top-[50%] left-3 sm:left-[3vw] xl:left-[15vw] text-sm sm:text-lg lg:text-2xl aspect-square w-8 sm:w-[50px] rounded-full text-white hover:border-2 border-white active:scale-90" onClick={() => handleLeft()}>←</button>
+      <button className="absolute top-[50%] right-3 sm:right-[3vw] xl:right-[15vw] text-sm sm:text-lg lg:text-2xl aspect-square w-8 sm:w-[50px] rounded-full text-white hover:border-2 border-white active:scale-90" onClick={() => handleRight()}>→</button>
     </div>
   )
 }
