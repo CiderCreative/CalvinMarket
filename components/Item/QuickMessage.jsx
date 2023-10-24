05253cf0de57b7f289bc7be8b17902f499528e1d
LoganHumphrey <113280028+lhumphrey3511@users.noreply.github.com>
2023-10-24 10:37:09 -0400

    A war (won) against the quickMessage component

--- components/Item/QuickMessage.jsx
+++ components/Item/QuickMessage.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 
 const QuickMessage = () => {
+  const [messageText, setMessageText] = useState("");
   const messageIcon = <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"/></svg>
+
+  function handleKeyPress(event) {
+    if (event.key === 'Tab' && messageText.length === 0) {
+      event.preventDefault();
+      setMessageText('Hello! Is this available?');
+    } else {
+      setMessageText(event.target.value);
+    }
+  }
+
   return (
     <div className="fixed w-full bottom-3">
 
@@ -10,7 +22,13 @@ const QuickMessage = () => {
         <p className="text-md font-regular">Send the seller a message</p>
       </div>
 
-      <input className="p-2 w-1/4 mt-1 mb-2 input-clear bg-opposite bg-opacity-10 border-2 border-opposite border-opacity-50 rounded-lg" defaultValue="Hello is this available?"></input>
+      <input
+      value={messageText}
+      onKeyDown={handleKeyPress}
+      onChange={(event) => setMessageText(event.target.value)}
+      placeholder="Hello is this available?"
+      className="p-2 w-1/4 mt-1 mb-2 input-clear bg-opposite bg-opacity-10 border-2 border-opposite border-opacity-50 rounded-lg"
+      />
 
       <button className="flex justify-center w-1/4 py-1 bg-yellow text-dark text-sm font-semibold rounded-lg">Send</button>
 
--- components/Item/SidebarMenu.jsx
+++ components/Item/SidebarMenu.jsx
@@ -3,7 +3,7 @@ import { ItemHeader, ItemDescription, SellerInfo, QuickMessage } from '.'
 
 const SidebarMenu = () => {
   return (
-    <div className="flex flex-col fixed right-0 inset-y-0 bg-red-200 w-[400px]">
+    <div className="flex flex-col fixed right-0 inset-y-0 w-[400px]">
       <ItemHeader />
       <ItemDescription />
       <SellerInfo />
