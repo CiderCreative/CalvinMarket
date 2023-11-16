bc3b17ada88027f284141d949828930c45bd53bf
LoganHumphrey <113280028+lhumphrey3511@users.noreply.github.com>
2023-11-16 13:43:15 -0500

    Dropdown toggle

--- components/EditListing/EditSidebarMenu.jsx
+++ components/EditListing/EditSidebarMenu.jsx
@@ -10,6 +10,7 @@ const EditSidebarMenu = () => {
   const [formValues, setFormValues] = useState({});
   const [files, setFiles] = useState([]);
   const [status, setStatus] = useState("unsent");
+  const [ dropdown, setDropdown ] = useState([])
 
   return (
     <div className="flex flex-col fixed right-0 inset-y-0 w-1/2 px-5 border-l-2 p-10 overflow-y-scroll">
@@ -42,75 +43,76 @@ const EditSidebarMenu = () => {
       {Object.keys(apparelType).map((key, index) => {
         let value = apparelType[key];
 
-          { // ------------- Radio Button Tags ------------- //
-          if (value.type === "button") {
-            return(
-            <div className='flex space-x-10 my-5'>
-              {/* Tag title */}
-              <p className="text-lg">{key}</p>
+        { // ------------- Radio Button Tags ------------- //
+        if (value.type === "button") {
+          return(
+          <div className='flex space-x-10 my-5' key={index}>
+            {/* Tag title */}
+            <p className="text-lg">{key}</p>
 
-              <div className="flex space-x-5">{value.options.map((option, index) => (
-                <div key={index}
-                  className="flex flex-col items-center"
-                  onClick={() => setFormValues({ ...formValues, [key]: option })}
-                >
-                  {/* Button */}
-                  <div className={`aspect-square rounded-full w-5 cursor-pointer border-[1px] border-opposite relative ${formValues[key] === option ? 'bg-yellow' : ''}`}>
-                    {/* Center dot*/}
-                    {formValues[key] === option && ( <div className="absolute inset-1/2 w-1.5 h-1.5 bg-black rounded-full transform -translate-x-1/2 -translate-y-1/2" />)}
-                  </div>
-
-                  {/* Description of option */}
-                  <label htmlFor={`${key}-option-${index}`} className="text-md cursor-pointer">{option}</label>
+            <div className="flex space-x-5">{value.options.map((option, index) => (
+              <div key={index}
+                className="flex flex-col items-center"
+                onClick={() => setFormValues({ ...formValues, [key]: option })}
+              >
+                {/* Button */}
+                <div className={`aspect-square rounded-full w-5 cursor-pointer border-[1px] border-opposite relative ${formValues[key] === option ? 'bg-yellow' : ''}`}>
+                  {/* Center dot*/}
+                  {formValues[key] === option && ( <div className="absolute inset-1/2 w-1.5 h-1.5 bg-black rounded-full transform -translate-x-1/2 -translate-y-1/2" />)}
                 </div>
-              ))}
 
+                {/* Description of option */}
+                <label htmlFor={`${key}-option-${index}`} className="text-md cursor-pointer">{option}</label>
+              </div>
+            ))}
+
+            </div>
+          </div>
+        )}
+
+        // ------------- Drop Down Tags ------------- //
+        else if (value.type === "drop-down")  {
+          return (
+          <div className="my-5">
+            <div className="flex items-center space-x-10">
+              <label className="text-lg">{key}</label>
+              <div
+                onClick={() => setDropdown(index)}
+                className="border-[1px] hover:bg-opposite/10 cursor-pointer border-opposite/30 rounded-xl px-5 py-2 flex justify-between items-center w-1/4"
+              >
+                {formValues[key] || 'Select an option'}
+                <ChevronDownIcon className="inset-y-0 right-0 pointer-events-none aspect-square w-6" />
               </div>
             </div>
+
+            <div>
+              {dropdown === index && value.options.map((option, index) => (
+                <div key={index}
+                  value={option}
+                  className="hover:bg-yellow cursor-pointer"
+                  onClick={() => setFormValues({ ...formValues, [key]: option })}
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+          </div>
           )}
 
-            // ------------- Drop Down Tags ------------- //
-          else if (value.type === "drop-down")  {
-              return (
-              <div className="my-5">
-                <div className="flex items-center space-x-10">
-                  <label className="text-lg">{key}</label>
-                  <div className="border-[1px] hover:bg-opposite/10 cursor-pointer border-opposite/30 rounded-xl px-5 py-2 flex justify-between items-center w-1/4">
-                    [Something]
-                    <ChevronDownIcon className="inset-y-0 right-0 pointer-events-none aspect-square w-6" />
-                  </div>
-                </div>
+        // ------------- Text Input Tags ------------- //
+        else if (value.type === "text"){
+          return (
+          <div>
+            <div>{key}</div>
+            <input
+              type="text"
+              placeholder={value.filler}
+              onChange={(e) => setFormValues({...formValues, [key]: e.target.value})}
+            />
+          </div>)
+          } }
 
-                <div>
-                  {value.options.map((option, index) => (
-                    <div key={index}
-                      value={option}
-                      className="hover:bg-yellow cursor-pointer"
-                      onClick={() => setFormValues({ ...formValues, [key]: option })}
-                    >
-                      {option}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-
-            // ------------- Text Input Tags ------------- //
-            else if (value.type === "text"){
-              return (
-              <div>
-                <div>{key}</div>
-                <input
-                  type="text"
-                  placeholder={value.filler}
-                  onChange={(e) => setFormValues({...formValues, [key]: e.target.value})}
-                />
-              </div>)
-             } }
-
-            })}
-
-
+        })}
 
 
       <FileInput files={files} setFiles={setFiles} />
